63c239c6271803215bd676bb05fb51659155e769
juancho <jm.ticona.p@gmail.com>
2022-10-29 00:33:13 -0500

    [ADD] insert statements

--- db.sql
+++ db.sql
@@ -36,3 +36,37 @@ constraint fk_post__post_category foreign key (post_id) references blog.public.p
 constraint fk_category__post_category foreign key (category_id) references blog.public.category(id)
 );
 
+insert into blog.public.user ("name","email","password","age")
+values 
+('Donovan','donovan@gmail.com','password',8),
+('Mia','mia.mauren@gmail.com','password',15),
+('Ada','ada.pacheco@gmail.com','password',74),
+('Godofredo','segundo.usa@hotmail.com','password',70);
+
+insert into blog.public.category ("name")
+values
+('Generico'),
+('Anuncio'),
+('Personal'),
+('Noticia'),
+('Trend'),
+('Politica'),
+('Deporte'),
+('Economía'),
+('Internacional'),
+('Cultura'),
+('Musica');
+
+insert into blog.public.post ("title","content" ,"author")
+values 
+('Cumpleaños de mi princesa','Este año fueron los 15 años de mi princesa',4);
+
+insert into blog.public.post_category ("post_id","category_id")
+values
+(1,3),
+(1,2);
+
+insert into blog.public.coment ("comment","post_id","author")
+values
+('Si fue el cumple de mi hermanita',1,1),
+('Mi nieta ya está grande',1,2);
